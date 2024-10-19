07a5caed6b60e2515bf052c0ed22c37a222d7962
Cristiano Gomes <101535034+CrisGaius@users.noreply.github.com>
2024-10-19 19:56:49 -0300

    Estilizando a lista de tarefas

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
-import { FaPlus } from 'react-icons/fa';
+import { FaPlus, FaEdit, FaWindowClose } from 'react-icons/fa';
 
 import './Main.css';
 
 export default class Main extends Component {
   state = {
     newTask: '',
+    tasks: [
+      'Read',
+      'Train',
+      'Study',
+    ],
   };
 
   inputChange = (e) => {
@@ -16,16 +21,28 @@ export default class Main extends Component {
   };
 
   render() {
-    const { newTask } = this.state;
+    const { newTask, tasks } = this.state;
 
     return (
       <div id="main">
-        <h1>Lista de Tarefas</h1>
+        <h1>To do List</h1>
 
         <form className="form">
           <input type="text" onChange={this.inputChange} value={newTask} />
           <button type="submit"><FaPlus /></button>
         </form>
+
+        <ul className="tasks">
+          {tasks.map((task) => (
+            <li key={task}>
+              {task}
+              <div className="buttons">
+                <FaEdit className="edit" />
+                <FaWindowClose className="delete" />
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
